1d9e5171cd0332dd55ae28cf42ebc29e35ef1668
giorgi002 <167451976+giorgi002@users.noreply.github.com>
2024-10-10 01:54:57 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,427 @@
+// "დამოუკიდებელი სამუშაო" 1(1)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     float A;
+//     int B,D;
+//     B = 8;
+//     D = 4;
+//     A = (B +D)/3.;
+//     cout << A << endl;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო" 1(2)
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int p,q,d ;
+//     p = 20;
+//     q= 8;
+//     d = p/4-q/6;
+//     cout << d << endl;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო" 1(3)
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     double p,q,d ;
+//     p = 20;
+//     q= 9;
+//     d = p/4-q/6;
+//     cout << d << endl;
+// }
+
+
+// "დამოუკიდებელი სამუშაო" 2(1)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x, y,d;
+//     x = 7;
+//     y = 9;
+//     d = y = (x == y);
+//     cout << d << endl;
+// }  
+
+
+
+// "დამოუკიდებელი სამუშაო" 2(2)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x, y;
+//     x = 7;
+//     y = 9;
+//     y = (x < y);
+//     cout << y << endl;
+// }
+
+
+
+// "დამოუკიდებელი სამუშაო" 2(3)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x, y;
+//     x = 7;
+//     y = 9;
+//     y = (x > y);
+//     cout << y << endl;
+// }
+
+
+// "დამოუკიდებელი სამუშაო" 2(4)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x, y;
+//     x = 7;
+//     y = 9;
+//     y = x <= y;
+//     cout << y << endl;
+// }
+
+
+
+//  "დამოუკიდებელი სამუშაო" 2(5)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x, y;
+//     x = 7;
+//     y = 9;
+//     y = x != y;
+//     cout << y << endl;
+// }
+
+
+
+//  "დამოუკიდებელი სამუშაო" 2(6)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x, y;
+//     x = 7;
+//     y = 9;
+//     y +=x;
+//     cout << y << endl;
+// }
+
+
+
+
+//  "დამოუკიდებელი სამუშაო" 2(7)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x, y;
+//     x = 7;
+//     y = 9;
+//     y *= x-1;
+//     cout << y << endl;
+// }
+
+
+
+//  "დამოუკიდებელი სამუშაო" 2(8)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x, y;
+//     x = 7;
+//     y = 9;
+//     y /= x +1;
+//     cout << y << endl;
+// }
+
+
+//  "დამოუკიდებელი სამუშაო" 2(9)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x, y;
+//     x = 7;
+//     y = 9;
+//     y += 2 *x - y;
+//     cout << y << endl;
+// }
+
+
+
+//  "დამოუკიდებელი სამუშაო" 3(1)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x =1, z;
+//     z = x++;
+//     cout << "x = " << x
+//     << "\tz = " << z << endl;
+// }
+
+
+
+//  "დამოუკიდებელი სამუშაო" 3(2)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x =1, z;
+//     z = ++x;
+//     cout << "x = " << x
+//     << " z = " << z << endl;
+// }
+
+
+
+//  "დამოუკიდებელი სამუშაო" 3(3)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     char s = 'B', t;
+//     t = s--;
+//     cout << "s = " << s 
+//     << "\tt = " << t << endl;
+// }
+
+
+
+// //  "დამოუკიდებელი სამუშაო" 3(4)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     char s = 'B', t;
+//     t = --s;
+//     cout << "s = " << s 
+//     << "\tt = " << t << endl;
+// }
+
+
+
+//  "დამოუკიდებელი სამუშაო" 3(5)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//    double d = 15.674, q;
+//    q = ++d -10;
+//    cout << "d = " << d
+//    << " q = " << q << endl;
+// }
+
+
+
+//  "დამოუკიდებელი სამუშაო" 3(6)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//    double d = 15.674, q;
+//    q = d-- +10;
+//    cout << "d = " << d
+//    << " q = " << q << endl;
+// }
+
+
+
+//  "დამოუკიდებელი სამუშაო" 4
+// #include <iostream>
+// #include <iomanip>
+// using namespace std;
+// int main(){
+//     cout << fixed << setprecision(20);
+//     cout << 3*0.1 <<endl;
+// }
+
+
+
+//  "დამოუკიდებელი სამუშაო" 5(1)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     string A = "stumari", B = "studenti";
+//     cout << ( A < B) << endl <<( A != B) << endl;
+// }
+
+
+
+//   "დამოუკიდებელი სამუშაო" 5(2)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     string C = "kargi";
+//     cout << ( C + "amindi" >= C + "ambavi") << endl;
+// }
+
+
+//   "დამოუკიდებელი სამუშაო" 5(3)
+// #include <iostream>
+// using namespace std;
+// int main(){ 
+//     string K = "Word", M = "Excel";
+//     cout << (K > M) << '\t';
+//     cout << (K = M) << endl;
+//     cout << (K == M) << endl;
+// }
+
+
+
+//   "დამოუკიდებელი სამუშაო" 6 (1)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     char p = '!', c = '?',d;
+//     d = p; p =c ; c = d;
+//     cout << p << ' ' << c << endl;
+// }
+
+
+
+//   "დამოუკიდებელი სამუშაო" 6 (2)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     bool t = true, f = false, d;
+//     d = f;
+//     f = t;
+//     t = d;
+//     cout << boolalpha << "t gaxda " << t
+//     <<endl << "f gaxda " << f << endl;
+// }
+
+
+
+//   "დამოუკიდებელი სამუშაო" 6 (3)
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     string S = "Gel", Q = "Euro", P;
+//     P = S; S = Q; Q = P;
+//     cout << S << '\t' << Q << endl;
+// }
+
+//   "დამოუკიდებელი სამუშაო" 7
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int s,m;
+//     cout << "what is m " << endl;
+//     cin >> m;
+//     s = 100 * (m%10) + 10 *(m/10%10) + (m/100);
+//     cout << "you numbers's reversed number is "<< s << endl;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+// }
+
+
+
+//   "დამოუკიდებელი სამუშაო" 9
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int N, reversed;
+//     cout << "What is N ?  " << endl;
+//     cin >> N;
+//     reversed = 100*(N%10) + 10*(N/10%10) + (N/100);
+//     cout << "reversed number is " << reversed << endl;
+// }
+
+
+
+//   "დამოუკიდებელი სამუშაო" 10(1)
+// #include <iostream>
+// #include <climits>
+// using namespace std; int main(){
+//    cout << INT_MIN << '\t' << INT_MAX << endl;
+//    cout << 0 << '\t' << UINT_MAX << endl;
+//    cout << CHAR_MIN << '\t' << CHAR_MAX << endl; 
+//    cout << 0 << '\t' << UCHAR_MAX << endl;
+//    cout << SHRT_MIN << '\t' << SHRT_MAX << endl;
+//    cout << 0 << '\t' << USHRT_MAX<<endl;
+//    cout << LONG_MIN << '\t' << LONG_MAX << endl; 
+//    cout << 0 << '\t' << ULONG_MAX << endl; 
+//    cout << sizeof(long long) << endl;
+//    cout << LLONG_MIN << '\t' << LLONG_MAX << endl;
+//    cout << 0 << '\t' << ULLONG_MAX << endl;   
+// }
+
+
+//   "დამოუკიდებელი სამუშაო" 10(2)
+// #include <iostream>
+// #include <cfloat>
+// using namespace std;
+// int main (){
+//     cout << "float min :"<<  FLT_MIN << '\t' << FLT_MAX << endl;
+//     cout << "double min :"<< DBL_MIN << '\t' << DBL_MAX << endl;
+// }
+
+
+
+//   "დამოუკიდებელი სამუშაო" 11
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+// int main()
+// {   
+//     int x = UINT_MAX, y = 0;
+//     cout << "x = " << x << '\t'
+//          << "x + 1 = " << x + 1 << '\t' 
+//          << "x + 2 = " << x + 2 << endl;
+//     cout << "y = " << y << '\t'
+//          << "y - 1 = " << y - 1 << '\t'
+//          << "y - 2 = " << y - 2 << endl;
+// }
+
+
+//   "დამოუკიდებელი სამუშაო" 13(1)
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "_______$_______" << endl;
+//     cout << "______$_$______" << endl;
+//     cout << "______$___$____" << endl;
+//     cout << "_____$_____$___" << endl;
+//     cout << "____$_______$__" << endl;
+//     cout << "____$_______$__" << endl;
+//     cout << "____$_______$__" << endl;
+//     cout << "____$_______$__" << endl;
+//     cout << "____$_______$__" << endl;
+//     cout << "____$_______$__" << endl;
+//     cout << "____$_______$__" << endl;
+//     cout << "___$__$___$__$_" << endl;
+//     cout << "__$__$_$_$_$__$__" << endl;
+//     cout << "_$__$__$$$__$__$_" << endl;
+//     cout << "_$$$____$____$$$_" << endl;
+//     cout << "_$______$______$_" << endl;
+//     cout << "$_______$_______$" << endl;
+
+// }
+//   "დამოუკიდებელი სამუშაო" 13(2)
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout << "_______________$$$$$$$$$$$$$$$$$$$" << endl;
+//     cout << "______________$$$$_$$$$$$$$$$$$$$$$$" << endl;
+//     cout << "____________$$$$_________$$$_______$$$" << endl;
+//     cout << "___________$$$___________$$$_________$$$" << endl;
+//     cout << "__________$$$$___________$$$__________$$$$" << endl;
+//     cout << "__$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << endl;
+//     cout << "_$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << endl;
+//     cout << "_$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << endl;
+//     cout << "__$$$$$$$$$___$$$$$$$$$$$$$$$$$$$$$$$$$___$$$$$$$$" << endl;
+//     cout << "_$$$$$$$$_______$$$$$$$$$$$$$$$$$$$$$_______$$$$$$" << endl;
+//     cout << "__$$$$$$__$$$$$__$$$$$$$$$$$$$$$$$$$__$$$$$$__$$$" << endl;
+//     cout << "_________$$___$$_____________________$$___$$__" << endl;
+//     cout << "_________$$___$$_____________________$$$__$$__" << endl;
+//     cout << "__________$$$$_________________________$$$$__" << endl;
+// }
+
+//    მერვე სავარჯიშო ვერ გავიგე !!!
